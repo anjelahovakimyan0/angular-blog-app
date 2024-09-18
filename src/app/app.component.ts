1da5258b280beb1072072cc440308d2f0c65b6c7
anjelahovakimyan0 <anjela.0hovakimyan@gmail.com>
2024-09-19 01:58:51 +0400

    footer and subscription components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./layouts/header/header.component";
+import {CategoryNavbarComponent} from "./layouts/category-navbar/category-navbar.component";
+import {FooterComponent} from "./layouts/footer/footer.component";
+import {PostCardComponent} from "./layouts/post-card/post-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CategoryNavbarComponent, FooterComponent, PostCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
